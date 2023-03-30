e6d5b9991d83c0ae30bc28f550e2b1b664154f08
Avishek Jana <avishek@geogo.in>
2023-03-30 18:13:50 +0530

    Step 9: Finishing touch - done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # omniauth
+  get '/auth/:provider/callback' => 'oauth_callbacks#create'
+  get '/auth/failure' => 'oauth_callbacks#failure'
+
+  # Custom logout
+  match '/logout', :to => 'oauth_callbacks#destroy', via: :all
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  
 end
